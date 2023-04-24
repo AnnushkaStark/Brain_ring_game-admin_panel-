73a857feff1d71a48b822c6cf4314b036ab8a568
Alexey(Krsk) <bobroferma@yandex.ru>
2023-04-24 12:35:59 +0700

    Написал терминалку на добавление и удаление вопросов из базы.

--- logic/term_app.py
+++ logic/term_app.py
@@ -1,36 +1,23 @@
-import sys
-
-from classes import questions
 import time
-from random import randint
+from SQLite.db_handlers import *
+import sqlite3 as sql
 
-print('For new game enter N')
-cm = input()
-if cm=='N':
-    print('Внимание вопрос!', end='')
-    time.sleep(2)
-    print('\r', end='')
-    print(questions[0].get_text(), end='')
-    time.sleep(5)
-
-print('\r', end='')
-print('Время пошло!', end='')
-time.sleep(1)
-_ = 60
-while _ > 0:
-    print('\r', end='')
-    print(f'{str(_)}', end='')
-    _-=1
-    time.sleep(1)
-
-print('\r', end='')
-print(f'Ваш ответ?')
-
-cm = input()
-
-print('\r', end='')
-print(f'Правильный ответ: {questions[0].get_answer()}')
-
-
-
-
+while True:
+    print('Если вы хотите добавить вопрос, введите: "add".')
+    print('Если вы хотите просмотреть имеющиеся вопросы, введите: "show".')
+    print('Если вы хотите изменить какой-то вопрос, введите: "edit".')
+    print('Если вы хотите удалить вопрос, введите: "delete".')
+    print('Если вы хотите закрыть модуль, введите: "exit".')
+    choise = input()
+    if choise=='exit':
+        break
+    elif choise=='add':
+        add_question()
+    elif choise == 'show':
+        show_question()
+    elif choise == 'edit':
+        pass
+    elif choise == 'delete':
+        delete_question()
+    else:
+        pass
