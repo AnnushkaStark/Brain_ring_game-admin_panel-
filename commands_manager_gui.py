3a1ce576a325063c4f4944add9afc203ff2315d6
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2023-10-09 23:09:17 +0300

    добавлены функции добавления команд в одиночнуюигру и в турнир

--- commands_manager_gui.py
+++ commands_manager_gui.py
@@ -13,8 +13,8 @@ class Commands(Ui_MainWindow,QMainWindow):
         self.setupUi(self)
         self.show()
         self.database = 'app/assets/database/brainring.db'
-        self.querry_add_single = '''INSERT INTO single_game (team_name, winners) VALUE (?,?)''' # Этот запрос регистрирует команду в одиноной игре
-        self.querry_add_single = '''INSERT INTO tournier (team_name, winners) VALUE (?,?)''' # Этот запрос регистрирует команду в турнире
+        self.querry_add_single = '''INSERT INTO single_game (team_name, winners) VALUES (?,?)''' # Этот запрос регистрирует команду в одиноной игре
+        self.querry_add_tournier = '''INSERT INTO tournier (team_name, winners) VALUES (?,?)''' # Этот запрос регистрирует команду в турнире
         self.querry_delet_single ='''DELET FROM single_game WHERE team_name LIKE ?''' # Этот запрос удаляет команду из рейтинга одиночной игры
         self.querry_delet_tourinier ='''DELET FROM tournier WHERE team_name LIKE ?''' # Этот запрос удаляет команду из рейтинга турнира
         self.querry_change_single = ''' UPDATE single_game SET team_name = ? WHERE team_name LIKE ?'''#Этот запрос измняет название команды в рейтинге одиночной игры
@@ -28,7 +28,34 @@ class Commands(Ui_MainWindow,QMainWindow):
         self.pushButton_delet_tournier.clicked.connect(self.delet_tournier)
 
     def add_single(self):
-        pass
+        ''''Эта функуция добавляет новую команду в  одиночную игру'''
+        try:
+            with sql.connect(self.database) as conn:
+                self.team_name = self.lineEdit_team_single.text()
+                self.raiting = 0
+                if self.team_name.strip():
+                    conn.cursor().execute(self.querry_add_single,(self.team_name,self.raiting))
+                    result = QMessageBox()
+                    result.setText(f'Команда {self.team_name} добавлена')
+                    result.exec()
+        except sql.IntegrityError:
+            result = QMessageBox()
+            result.setText('Error')
+            result.exec()
+        except sql.OperationalError:
+            result = QMessageBox()
+            result.setText('Error')
+            result.exec()
+        except TypeError:
+            result = QMessageBox()
+            result.setText('Error')
+            result.exec()
+        except ValueError:
+            result = QMessageBox()
+            result.setText('Error')
+            result.exec()
+
+
 
     def change_single(self):
         pass
@@ -37,7 +64,34 @@ class Commands(Ui_MainWindow,QMainWindow):
         pass
 
     def add_tournier(self):
-        pass
+        '''Эта функция добавляет команду в турнир'''
+        try:
+            with sql.connect(self.database) as conn:
+                self.team_name = self.lineEdit_team_tournier.text()
+                self.raiting = 0
+                if self.team_name.strip():
+                    conn.cursor().execute(self.querry_add_tournier,(self.team_name,self.raiting))
+                    result = QMessageBox()
+                    result.setText(f'Команда {self.team_name} добавлена')
+                    result.exec()
+        except sql.IntegrityError:
+            result = QMessageBox()
+            result.setText('Error')
+            result.exec()
+        except sql.OperationalError:
+            result = QMessageBox()
+            result.setText('Error')
+            result.exec()
+        except TypeError:
+            result = QMessageBox()
+            result.setText('Error')
+            result.exec()
+        except ValueError:
+            result = QMessageBox()
+            result.setText('Error')
+            result.exec()
+
+
 
     def change_tournier(self):
         pass
